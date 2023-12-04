1434f0daad8270ad488ebe536c9d58ede94fcc1a
krishna saini <krishnapraanjal10@gmail.com>
2023-12-04 05:39:58 +0530

    exponent of a number using recursion

--- 1basic_recursion/2power.js
+++ 1basic_recursion/2power.js
@@ -2,4 +2,7 @@ function power(base, exponent){
     if(exponent === 0) return 1;
     return base * power(base, exponent-1);
 }
-console.log(power(-2, 3)); // 1
+console.log(power(-2, 3)); 
+
+// TC: O(1)*O(n) = O(n)     // O(1) for each recursive call and there are n recursive calls
+// SC: O(n)   // n is the depth of the recursion tree
