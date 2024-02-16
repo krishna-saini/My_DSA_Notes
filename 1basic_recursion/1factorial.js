0690ed83f5ce0d8203a1a9e7e808630f8e3366f8
krishna saini <krishnapraanjal10@gmail.com>
2024-02-16 08:38:55 +0530

    factorial using loop

--- 1basic_recursion/1factorial.js
+++ 1basic_recursion/1factorial.js
@@ -1,3 +1,14 @@
+const fact = (n) => {
+  let ans = 1;
+  for (let i = 1; i <= n; i++) {
+    ans = ans * i;
+  }
+  return ans;
+};
+
+// TC - O(n)
+// SC - O(1)
+
 /**
  * find factorial of a given integer using recursion
  *
@@ -13,3 +24,15 @@ function factorial(n) {
 
   return n * factorial(n - 1);
 }
+
+// time complexity
+// space complexity ->
+/**
+ * The space complexity of the recursive solution is O(n) due to the recursive call stack.
+ *  In each recursive call, a new frame is added to the call stack,
+ * storing the local variables and the current state of the function.
+ * The maximum depth of the call stack is equal to the value of 'n' (the target sum).
+ * Therefore, the space required on the call stack is proportional to the value of 'n'.
+ */
+
+// futher optimisation can be done using DP/memoisation
