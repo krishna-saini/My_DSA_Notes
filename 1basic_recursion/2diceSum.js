8331fbd5dacb0b9b10cf7a3ac4b129a0919e3f72
krishna saini <krishnapraanjal10@gmail.com>
2024-02-17 11:44:40 +0530

    dice combination using DP

--- 1basic_recursion/2diceSum.js
+++ 1basic_recursion/2diceSum.js
@@ -6,25 +6,24 @@
  * 2+1
  * 1+2
  * 3
- * 
+ *
  * https://cses.fi/problemset/task/1633
  */
 
 /**
  * Brute Force Approach
  */
-function countWaysToConstructSumFromDice(sum){
-    if(sum === 0) return 1;
-    if(sum < 0) return 0;
-    
-    let ans = 0;
-    for(let i = 1; i <= 6; i++){
-        ans += countWaysToConstructSumFromDice(sum - i);
-    }
-    return ans; 
+function countWaysToConstructSumFromDice(sum) {
+  if (sum === 0) return 1;
+  if (sum < 0) return 0;
+
+  let ans = 0;
+  for (let i = 1; i <= 6; i++) {
+    ans += countWaysToConstructSumFromDice(sum - i);
+  }
+  return ans;
 }
 console.log(countWaysToConstructSumFromDice(3));
-
 
 // TC: O(6^n)  // 6 choices for each dice where "n" is the value of the input sum
 //SC: O(n)  // n is the depth of the recursion tree
@@ -40,10 +39,38 @@ console.log(countWaysToConstructSumFromDice(3));
 // hence total number of ways = 6^n
 
 /**
- * The reason for the exponential time complexity is that the function explores a branching tree of recursive calls,
- * with each level of the tree representing a die throw and each branch representing a different outcome of the die.
- * The number of recursive calls grows exponentially with the value of sum, making the time complexity exponential.
+ * The reason for the exponential time complexity is that the function explores a
+ * branching tree of recursive calls, with each level of the tree representing a
+ * dice throw and each branch representing a different outcome of the die.
+ * The number of recursive calls grows exponentially with the value of sum,
+ * making the time complexity exponential.
  */
 
 // how to improve TC?
 // DP
+
+const countWaysToConstructSumFromDiceMemo = (n, memo = {}) => {
+  if (n < 0) return 0;
+  // base case: if n is zero, there will be only one throw
+  if (n === 0) return 1; // O(1)
+  if (memo[n]) {
+    return memo[n];
+  }
+  let count = 0;
+  for (let i = 1; i <= 6; i++) {
+    count += countWaysToConstructSumFromDiceMemo(n - i, memo);
+  }
+  memo[n] = count;
+  console.log(memo);
+  return memo[n];
+};
+console.log(countWaysToConstructSumFromDiceMemo(6));
+
+// TC
+// computation of base case C(0) = O(1)
+// C(1) = C(0)
+// C(2) = C(1)+C(0) O(1)
+// C(n) = sum(i=1 to i=6)C(n-i) where this will be either equal to computations we have
+// done above or can be ignored if n-i < 0. 
+// hence overall TC O(n)
+
