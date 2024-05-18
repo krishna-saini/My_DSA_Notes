bc57951156c0ba902c19250e87d8e64a5cadad3b
krishna saini <krishnapraanjal10@gmail.com>
2024-05-18 07:33:23 +0530

    get, set reverse insert delete of LL

--- 6linkedlist/singleLL.js
+++ 6linkedlist/singleLL.js
@@ -76,13 +76,126 @@ class singlyLL {
     return this;
   }
 
+  // retrieve a node by its position in the linked list
+  // list(2) -> {val: 3, next: next}
+  get(index) {
+    if (index < 0) return undefined;
+    if (index > this.length) return undefined;
+    if (!this.head) return undefined;
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    // 2nd way
+    // let ans = currentNode;
+    // while(currentNode.next){
+    //   currentNode = currentNode.next;
+    //   counter++;
+    //    if(counter === index){
+    //     ans = currentNode;
+    //     break;
+    //   }
+    // }
+    // return ans;
+    return currentNode;
+  }
+
+  /**
+   * change the value of a node based on its index in the LL
+   * it accepts a value and an index
+   * use the get functin to find the specific node
+   * if node not found, return undefined/false;
+   * else change the value of that node and return the list/true
+   */
+  set(newValue, index) {
+    const requiredNode = this.get(index);
+    if (!requiredNode) return false;
+    requiredNode.val = newValue;
+    return this;
+  }
+
+  /**
+   * Insert - adding a node to the LL at a specific position
+   * accepts a index and a value
+   * if index < 0 ||  > this.length, return false
+   * if index === this.length, work like push
+   */
+  insert(newValue, index) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) {
+      return this.unshift(newValue);
+    }
+    if (index === this.length) {
+      return this.push(newValue);
+    }
+
+    const newNode = new Node(newValue);
+    let preNode = this.get(index - 1);
+    newNode.next = preNode.next;
+    preNode.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  /**
+   * remove a node from specific index
+   * it accepts a index
+   * if index < 0 || index > length, return false
+   * if index === length, do pop
+   * if index === 0, do shift
+   * else remove the node
+   */
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+
+    const preNode = this.get(index - 1);
+    const removedNode = preNode.next;
+    preNode.next = removedNode.next;
+
+    this.length--;
+    return removedNode;
+  }
+
+  /**
+   * Reverse the LL in place
+   */
+  reverse() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let next = null;
+    let prev = null;
+    while (current) {
+      next = current.next;
+
+      current.next = prev;
+
+      prev = current;
+      current = next;
+  
+    }
+
+    // Swap head and tail
+    this.tail = this.head;
+    this.head = prev;
+    return this;
+  }
 }
 
 const list = new singlyLL();
 list.push('Hi');
+list.push('there');
 list.push('krishna');
-console.log('krishna1', list);
-list.shift();
-console.log('krishna2', list);
-list.unshift('kartik');
-console.log('krishna3', list);
+list.push('Saini');
+// console.log('krishna1', list);
+// list.shift();
+// console.log('krishna2', list);
+// list.unshift('kartik');
+console.log('krishna3', list.reverse());
