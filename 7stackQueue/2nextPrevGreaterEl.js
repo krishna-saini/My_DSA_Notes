17c87706f8328649344c6707c265b1ff72213604
krishna saini <krishnapraanjal10@gmail.com>
2024-06-03 08:05:26 +0530

    next previous greater smaller element of a array

---
+++ 7stackQueue/2nextPrevGreaterEl.js
@@ -0,0 +1,47 @@
+/**
+ * Given an array, find the next greater/smaller element for each element.
+ * The next greater element of an element is the first greater element to its right.
+ */
+
+var nextSmaller = function (numbers) {
+  const stack = [];
+  const result = new Array(numbers.length).fill(0);
+
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length && numbers[i] < numbers[stack[stack.length - 1]]) {
+      const index = stack.pop();
+      result[index] = numbers[i];
+    }
+
+    stack.push(i);
+  }
+  return result;
+};
+
+/**
+ * Given an array, find the previous greater/smaller element for each element.
+ * The next greater element of an element is the first greater element to its right.
+ */
+
+/**
+ * Intuition -
+ * We maintain a stack that keeps track of the indices of elements
+ * whose previous greater element is yet to be found.
+ */
+const previousGreater = (numbers) => {
+  const stack = [];
+  const result = new Array(numbers).fill(-1);
+
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length > 0 && numbers[i] >= numbers[stack[stack.length - 1]]) {
+      stack.pop();
+    }
+    if (stack.length > 0) {
+      result[i] = numbers[stack[stack.length - 1]];
+    }
+
+    stack.push(i);
+  }
+
+  return result;
+};
