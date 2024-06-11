f2c2fa1f206811cedc058d4d67d5d6fd4a597f2f
krishna saini <krishnapraanjal10@gmail.com>
2024-06-11 07:41:55 +0530

    implement bfs for general tree

--- Design_DS_with_test_case/tree/1Traversal/bfs.js
+++ Design_DS_with_test_case/tree/1Traversal/bfs.js
@@ -25,7 +25,7 @@ export function bfsTree(rootNode) {
 
   // While there are nodes to process in the queue
   while (queue.length) {
-     // Dequeue the front node from the queue & add it in result
+    // Dequeue the front node from the queue & add it in result
     const currentNode = queue.shift();
     result.push(currentNode.value);
 
@@ -41,10 +41,44 @@ export function bfsTree(rootNode) {
   return result;
 }
 
-// TC - O(n) 
+// TC - O(n)
 // Each node is enqueued and dequeued exactly once. While shift() is O(n), it doesn't dominate the overall complexity
 
 // Space Complexity - O(n)
 // The maximum number of nodes in the queue is proportional to the max width of the tree, which in the worst case could be O(n)
 // Result Array: This will store  n node values. O(n)
 
+// above BFS is valid for binary tree
+// make it extensible to any tree
+
+export class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.children = []; // Array to hold child nodes
+  }
+
+  /**
+   * Adds a child node to this node.
+   */
+  addChild(child) {
+    this.children.push(child);
+  }
+}
+
+export const genericBFS = (rootNode) => {
+  if (!rootNode) return [];
+
+  const queue = [rootNode];
+  const result = [];
+
+  while (queue.length) {
+    let currentNode = queue.shift();
+    result.push(currentNode.value);
+
+    // Enqueue all children of the current node
+    for (const child of currentNode.children) {
+      queue.push(child);
+    }
+  }
+  return result;
+};
--- Design_DS_with_test_case/tree/1Traversal/bfs.test.js
+++ Design_DS_with_test_case/tree/1Traversal/bfs.test.js
@@ -1,4 +1,4 @@
-import { Node, bfsTree } from "./bfs";
+import { Node, TreeNode, bfsTree, genericBFS } from "./bfs";
 
 describe('test BFS tree operation', ()=>{
     let rootNode;
@@ -24,3 +24,32 @@ describe('test BFS tree operation', ()=>{
     expect(result).toEqual([]);
   });
 })
+
+
+describe('BFS General Tree Operation', () => {
+  let rootNode;
+
+  beforeEach(() => {
+    rootNode = new TreeNode(1);
+    const child1 = new TreeNode(2);
+    const child2 = new TreeNode(3);
+    const child3 = new TreeNode(4);
+    rootNode.addChild(child1);
+    rootNode.addChild(child2);
+    rootNode.addChild(child3);
+    child1.addChild(new TreeNode(5));
+    child1.addChild(new TreeNode(6));
+    child2.addChild(new TreeNode(7));
+    child3.addChild(new TreeNode(8));
+  });
+
+  test('returns correct BFS order for a general tree', () => {
+    const result = genericBFS(rootNode);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  test('returns empty array for an empty tree', () => {
+    const result = genericBFS(null);
+    expect(result).toEqual([]);
+  });
+});
