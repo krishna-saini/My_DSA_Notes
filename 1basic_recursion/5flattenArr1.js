4fe59f1cb2fa890ce51a8ebf82137cc445c290d1
krishna saini <krishnapraanjal10@gmail.com>
2024-07-01 05:45:56 +0530

    flatten deeply nested  array into single array

---
+++ 1basic_recursion/5flattenArr1.js
@@ -0,0 +1,17 @@
+const flattenArray = (inputArray) => {
+  if (!Array.isArray(inputArray)) {
+    return 'Error: Input is not an array';
+  }
+  const result = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (typeof inputArray[i] === 'number') {
+      result.push(inputArray[i]);
+    } else {
+      const flattenedSubArray = flattenArray(inputArray[i]);
+      result.push(...flattenedSubArray);
+    }
+  }
+  return result;
+};
+
+console.log(flattenArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
