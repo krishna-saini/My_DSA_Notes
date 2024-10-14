43cf5175eb5d13d63a7eab7248ae230163f1a728
krishna saini <krishnapraanjal10@gmail.com>
2024-10-14 08:15:55 +0530

    binary search problems

--- 4searching/binary_search/easy/4.searchInsertProblem.js
+++ 4searching/binary_search/easy/4.searchInsertProblem.js
@@ -7,7 +7,7 @@ Given a sorted array of distinct integers and a target value,
 */
 
 // Brute force
-var searchInsert = function (nums, target) {
+const searchInsert = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === target) {
       return i;
@@ -17,26 +17,58 @@ var searchInsert = function (nums, target) {
   }
   return nums.length;
 };
-console.log("brute force", searchInsert([1,2,4,5,6,7], 6));
-console.log("brute force", searchInsert([1,2,4,5,6,7], 8));
+console.log('brute force', searchInsert([1, 2, 4, 5, 6, 7], 6));
+console.log('brute force', searchInsert([1, 2, 4, 5, 6, 7], 8));
 
-const searchInsertOptimal = (nums, target)=>{
+const searchInsertOptimal = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
 
   while (left <= right) {
-      let mid = left + Math.floor((right - left) / 2);
-      if (nums[mid] === target) {
-          return mid;
-      } else if (nums[mid] < target) {
-          left = mid + 1;
-      } else {
-          right = mid - 1;
-      }
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
+  // If the target is not found, return the insertion index
+  return left; // Insertion index
+};
 
-  return nums.length; // Insertion index
-}
+console.log('optimal force', searchInsertOptimal([1, 2, 4, 5, 6, 7], 3));
+console.log('optimal force', searchInsertOptimal([1, 2, 4, 5, 6, 7], 8));
 
-console.log("optimal force", searchInsertOptimal([1,2,4,5,6,7], 6));
-console.log("optimal force", searchInsertOptimal([1,2,4,5,6,7], 8));
+// followup -> what if there are duplicate elements
+//. If it exists multiple times, return the index of the first occurrence.
+// If the target does not exist,
+//return the position where it would be inserted to maintain the order.
+
+const searchInsertOptimalModified = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      if (nums[mid - 1] !== target) {
+        return mid;
+      } else {
+        right = mid - 1;
+      }
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  // If the target is not found, return the insertion index
+  return left; // Insertion index
+};
+
+console.log(
+  'optimal force modified',
+  searchInsertOptimalModified([1, 5, 5, 5, 5, 6, 7], 5)
+);
--- 4searching/binary_search/medium/1.SearchInRotatedArray.js
+++ 4searching/binary_search/medium/1.SearchInRotatedArray.js
@@ -17,12 +17,26 @@ Output: -1
 
 // Optimised using Binary search as array is sorted
 /*
-Though the array is rotated, we can clearly notice that for every index,
-one of the 2 halves will always be sorted. In the above example,
-the right half of the index mid is sorted.
+Intuition for Solving the Problem:
+Since the array is rotated, it breaks into two sorted subarrays. 
+To efficiently search for the target element, you can apply the following strategy:
 
-If arr[low] <= arr[mid]: In this case, we identified that the left half is sorted.
-If arr[mid] <= arr[high]: In this case, we identified that the right half is sorted.
+1. Identify the Sorted Half: 
+   During each iteration of the binary search, 
+   determine which half of the array is sorted.
+   This is possible because, even after rotation, 
+   one of the two halves will always be sorted.
+
+   If arr[low] <= arr[mid]: In this case, we identified that the left half is sorted.
+   If arr[mid] <= arr[high]: In this case, we identified that the right half is sorted.
+
+2. Determine Which Half to Search: 
+   Once you identify the sorted half, 
+   you can decide whether the target lies in that half or 
+   the other half. Based on that, you adjust your search range, 
+   just like in a standard binary search.
+
+
 */
 
 /**
@@ -31,37 +45,36 @@ If arr[mid] <= arr[high]: In this case, we identified that the right half is sor
  * @return {number}
  */
 var search = function (arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left <= right) {
-      const mid = left + Math.floor((right - left) / 2);
-      console.log('k', left, right, mid, arr[mid]);
-      if (arr[mid] === target) {
-        return mid;
-      }
-      if (arr[left] <= arr[mid]) {
-        // left half is sorted
-        // check if target exist
-        if (arr[left] <= target && target <= arr[mid]) {
-          // discard right half
-          right = mid - 1;
-        } else {
-          // discard left half
-          left = mid + 1;
-        }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    console.log('k', left, right, mid, arr[mid]);
+    if (arr[mid] === target) {
+      return mid;
+    }
+    if (arr[left] <= arr[mid]) {
+      // left half is sorted
+      // check if target exist
+      if (arr[left] <= target <= arr[mid]) {
+        // discard right half
+        right = mid - 1;
       } else {
-        // right half is sorted
-        // check if target exist
-        if (arr[mid] <= target && target <= arr[right]) {
-          // discard left half
-          left = mid + 1;
-        } else {
-          // discard right half
-          right = mid - 1;
-        }
+        // discard left half
+        left = mid + 1;
+      }
+    } else {
+      // right half is sorted
+      // check if target exist
+      if (arr[mid] <= target <= arr[right]) {
+        // discard left half
+        left = mid + 1;
+      } else {
+        // discard right half
+        right = mid - 1;
       }
     }
-    return -1;
-  };
-    console.log(search([1, 3], 1));
-  
+  }
+  return -1;
+};
+console.log(search([1, 3], 1));
