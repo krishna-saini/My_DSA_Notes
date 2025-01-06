bf48965c5742b37b638cca790987e74221d53317
krishna-saini <krishnapraanjal10@gmail.com>
2025-01-06 07:01:59 +0530

    solve buy and sell stock using sliding window

--- 3array/easy/7buy&SellStock.js
+++ 3array/easy/7buy&SellStock.js
@@ -23,6 +23,12 @@ Output: 0
 // for such problems, keep track of minPrice and
 // then keep on updating maxProfite in each iteration
 /**
+ * maxProfit = max(sellPrice - buyPrice)
+ *           = max SellPrice - min buyPrice
+ * Keep track of min Value while looping (greddy )
+ * along with that keep track of maxProfit
+ */
+/**
  * @param {number[]} prices
  * @return {number}
  */
@@ -35,4 +41,100 @@ var maxProfit = function (arr) {
   }
   return maxP;
 };
-console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+
+// solve above  using sliding window
+const maxProfit2 = function (prices) {
+  if (prices.length < 2) {
+    return 0;
+  }
+  let buyP = 0;
+  let sellP = 1;
+  let maxProfit = 0;
+  while (sellP < prices.length) {
+    // check if we are profitable with given pointer
+    if (prices[sellP] > prices[buyP]) {
+      const profit = prices[sellP] - prices[buyP];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      // it means we have found a lower or equal value on right side
+      // so update our buying pointer
+      buyP = sellP;
+    }
+
+    // update sellP always
+    sellP++;
+  }
+
+  return maxProfit;
+};
+// 121. Best Time to Buy and Sell Stock
+
+/**
+ * You are given an array prices where prices[i] is the price of 
+ * a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and 
+choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction.
+ If you cannot achieve any profit, return 0.
+
+ Input: prices = [7,1,5,3,6,4]
+Output: 5
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+ */
+
+// brute force
+// O(n2) O(1)
+
+// for such problems, keep track of minPrice and
+// then keep on updating maxProfite in each iteration
+/**
+ * maxProfit = max(sellPrice - buyPrice)
+ *           = max SellPrice - min buyPrice
+ * Keep track of min Value while looping (greddy )
+ * along with that keep track of maxProfit
+ */
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (arr) {
+  let maxP = 0;
+  let minPrice = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    minPrice = Math.min(arr[i], minPrice);
+    maxP = Math.max(maxP, arr[i] - minPrice);
+  }
+  return maxP;
+};
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+
+// solve above  using sliding window
+const maxProfit2 = function (prices) {
+  if (prices.length < 2) {
+    return 0;
+  }
+  let buyP = 0;
+  let sellP = 1;
+  let maxProfit = 0;
+  while (sellP < prices.length) {
+    // check if we are profitable with given pointer
+    if (prices[sellP] > prices[buyP]) {
+      const profit = prices[sellP] - prices[buyP];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      // it means we have found a lower or equal value on right side
+      // so update our buying pointer
+      buyP = sellP;
+    }
+
+    // update sellP always
+    sellP++;
+  }
+
+  return maxProfit;
+};
--- Neetcode150/9buySellStocks.js
+++ Neetcode150/9buySellStocks.js
@@ -20,39 +20,38 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
  * along with that keep track of maxProfit
  */
 const maxProfit = (arr) => {
-    let maxP = 0;
-    // Greddy- updates the 'buy' price to the lowest seen so far, and updates the profit if a better opportunity is found.
-    let minPrice = Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        minPrice = Math.min(arr[i], minPrice);
-        maxP = Math.max(maxP, arr[i] - minPrice);
-    }
-    return maxP;
+  let maxP = 0;
+  // Greddy- updates the 'buy' price to the lowest seen so far, and updates the profit if a better opportunity is found.
+  let minPrice = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    minPrice = Math.min(arr[i], minPrice);
+    maxP = Math.max(maxP, arr[i] - minPrice);
+  }
+  return maxP;
 };
 
 // solve above  using sliding window
 const maxProfit2 = function (prices) {
-    if (prices.length < 2) {
-      return 0;
+  if (prices.length < 2) {
+    return 0;
+  }
+  let buyP = 0;
+  let sellP = 1;
+  let maxProfit = 0;
+  while (sellP < prices.length) {
+    // check if we are profitable with given pointer
+    if (prices[sellP] > prices[buyP]) {
+      const profit = prices[sellP] - prices[buyP];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      // it means we have found a lower or equal value on right side
+      // so update our buying pointer
+      buyP = sellP;
     }
-    let buyP = 0;
-    let sellP = 1;
-    let maxProfit = 0;
-    while (sellP < prices.length) {
-      // check if we are profitable with given pointer
-      if (prices[sellP] > prices[buyP]) {
-        const profit = prices[sellP] - prices[buyP];
-        maxProfit = Math.max(maxProfit, profit);
-      } else {
-        // it means we have found a lower or equal value on right side
-        // so update our buying pointer
-        buyP = sellP;
-      }
-  
-      // update sellP always
-      sellP++;
-    }
-  
-    return maxProfit;
+
+    // update sellP always
+    sellP++;
+  }
+
+  return maxProfit;
 };
-  
